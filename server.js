56b1cf1c16a3e7f6e0bdf03ef498788ed66cefd3
tcee42 <toochukwuali@gmail.com>
2020-09-15 23:38:05 +0100

    express setup

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello basic auth')
+}) 
+
+app.listen(8000, () => {
+    console.log("Server is running on port 8000")
+})
