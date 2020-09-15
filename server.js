29783fd867bf9cc622e8de6bb4c208c951c75c72
tcee42 <toochukwuali@gmail.com>
2020-09-16 00:52:46 +0100

    basic setup complete

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
+const pug = require('pug')
+
+app.set("view engine", "pug")
 
 app.get('/', (req, res) => {
-    res.send('Hello basic auth')
-}) 
+    res.render("index")
+})
+
+app.get('/register', (req, res) => {
+    res.render("register")
+})
+
+app.get('/login', (req, res) => {
+    res.render("login")
+})
 
 app.listen(8000, () => {
     console.log("Server is running on port 8000")
