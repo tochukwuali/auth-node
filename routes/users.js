4fb59b452d3bf95d1ca5c5dad28741c2c8a46451
tcee42 <toochukwuali@gmail.com>
2020-09-17 21:22:27 +0100

    express-session

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const Router = express.Router()
 const User = require('../models/users')
+const bcrypt = require('bcryptjs')
+
+const sessionizeUser = user => {
+  return { userId: user.id, firstname: user.firstname };
+}
 
 Router
     .route('/')
@@ -14,8 +19,19 @@ Router
     })
     .post(async (req, res) => {
         try {
-            await User.create(req.body)
-            res.redirect("dashboard")
+            let { firstname, lastname, email, password } = req.body
+
+            let hash = bcrypt.hashSync(password, 14)
+            password = hash
+
+            const newUser = new User({ firstname, lastname, email, password });
+            const sessionUser = sessionizeUser(newUser);
+            await newUser.save()
+
+            req.session.user = sessionUser;
+            console.log(req.session);
+            //res.send(sessionUser);
+            res.redirect("/dashboard")
         } catch (error) {
             console.log(error)
             res.send('error/500')
@@ -35,13 +51,15 @@ Router
                 return res.status(404).json({ emailnotfound: "Email not found" });
                 }  
 
-                if (req.body.password !== user.password) {
+                if (!bcrypt.compareSync(req.body.password, user.password)) {
                     return res.render("login", {
-                        error: "Incorrect email/Password"
+                        error: "Incorrect Password"
                     }) 
-                } else {
-                    res.redirect("dashboard")
-                }
+                }   
+                const sessionUser = sessionizeUser(user);
+                req.session.user = sessionUser;
+                console.log(req.session);
+                res.redirect("/dashboard")
             })
         } catch (error) {
             
@@ -50,8 +68,44 @@ Router
 
 Router
     .route('/dashboard')
-    .get((req, res) => {
-        res.render("dashboard")
+    .get(async (req, res, next) => {
+        try {
+            if (!(req.session && req.session.user)) {
+            console.log(req.session.user)
+            return res.redirect("/login")
+        } 
+
+       await User.findById({_id: req.session.user.userId}).then((user) => {
+            if (!user) {
+              return res.redirect("/login");
+            }
+            console.log(req.session.user);
+            res.render("dashboard");
+        })
+        } catch (error) {
+            console.log(error)
+        }
+        
     })
+
+    Router
+        .route('/logout')
+        .delete(async ({session}, res, next) => {
+            try {
+              const user = session.user;
+              if (user) {
+                session.destroy(err => {
+                  if (err) throw err;
+                  res.clearCookie(process.env.SESS_NAME);
+                  console.log(session)
+                  res.redirect("/login");
+                });
+              } else {
+                throw new Error("Something went wrong");
+              }
+            } catch (err) {
+              res.status(422).send(parseError(err));
+            }
+        })
 
 module.exports = Router; 
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 const connectDB= require('./config/db')
 const userRoute = require('./routes/users.js')
+const session = require('express-session')
+const connectStore = require("connect-mongo")
 
 dotenv.config({path: 'config/config.env' })
 
 connectDB()
 
 const app = express();
+const MongoStore = connectStore(session);
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
@@ -16,6 +20,25 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
+
+app.use(
+  session({
+    name: process.env.SESS_NAME,
+    secret: process.env.SESS_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      collection: "session",
+      ttl: parseInt(process.env.SESS_LIFETIME) / 1000
+    }),
+    cookie: {
+      sameSite: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: parseInt(process.env.SESS_LIFETIME)
+    }
+  })
+);
 
 app.set("view engine", "pug")
 
